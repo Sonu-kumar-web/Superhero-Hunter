fdbc17a6afb82914cf934a7772cb10bf88e9074b
Sonu <sonu3660@gmail.com>
2020-09-08 00:41:33 +0530

    Add favoutite.js file

--- instant-search/js/favorite-hero.js
+++ instant-search/js/favorite-hero.js
@@ -1,8 +1,8 @@
 var mainDiv = document.getElementById("main-div");
-/*This loop will iterate in the local storage. Makes a html (using the values of key in local storage) and place in the main div*/
-Object.keys(localStorage).forEach(function(key) {
-    var heroDetail = JSON.parse(localStorage.getItem(key));
-    const item = `<div class="card">
+
+Object.keys(localStorage).forEach(function (key) {
+   var heroDetail = JSON.parse(localStorage.getItem(key));
+   const item = `<div class="card">
                         <div class="image">
                             <img src="${heroDetail.image.url}">
                         </div>
@@ -15,23 +15,25 @@ Object.keys(localStorage).forEach(function(key) {
                             <p>Gender : ${heroDetail.appearance.gender}</p>
                             <p>Height : ${heroDetail.appearance.height[0]}</p>
                             <p>Weight : ${heroDetail.appearance.weight[1]}</p>
-                            <p>Eye Color : ${heroDetail.appearance['eye-color']}</p>
-                            <p>Hair Color : ${heroDetail.appearance['hair-color']}</p>
+                            <p>Eye Color : ${heroDetail.appearance["eye-color"]}</p>
+                            <p>Hair Color : ${heroDetail.appearance["hair-color"]}</p>
                             <p>Publisher : ${heroDetail.biography.publisher}</p>
                             <button id = ${heroDetail.id} >Unavorite</button>
                         </div>
-                    </div>`
+                    </div>`;
 
-    const position = "beforeend";
-    mainDiv.insertAdjacentHTML(position, item);
+   const position = "beforeend";
+   mainDiv.insertAdjacentHTML(position, item);
 });
-/*Function will handle click on unfavorite button*/
-mainDiv.addEventListener('click', () => {
-        var heroId = event.target.id
-        event.target.parentNode.parentNode.parentNode.removeChild(event.target.parentNode.parentNode);
-        localStorage.removeItem(heroId);
-    })
-    /*Function handel the search btn click*/
-document.getElementById("search-btn").addEventListener('click', () => {
-    location.replace("./index.html");
-})
+/*Function will handle click on un-favorite button*/
+mainDiv.addEventListener("click", () => {
+   var heroId = event.target.id;
+   event.target.parentNode.parentNode.parentNode.removeChild(
+      event.target.parentNode.parentNode
+   );
+   localStorage.removeItem(heroId);
+});
+/*Function handel the search btn click*/
+document.getElementById("search-btn").addEventListener("click", () => {
+   location.replace("./index.html");
+});
